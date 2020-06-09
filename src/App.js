e98d068ea31503e41f773273ba05a8aab5776826
usman23499 <64378519+usman23499@users.noreply.github.com>
2020-06-09 11:03:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -9,8 +9,7 @@ function App() {
        </h1>
        <h1>My name is Muhamad Usman Hussain
        </h1>
-       <h1>My name is Muhamad Usman Hussain
-       </h1>
+       
 
     </div>
   );
